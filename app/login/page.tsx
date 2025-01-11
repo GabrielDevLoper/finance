d277c8041835439da6af20d6e7d9da531fc455ae
gabrielbarreto <gabriel.devloper@gmail.com>
2025-01-11 01:43:26 -0300

    feat: install clerk service auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Login = () => {
+const Login = async () => {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/");
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       <div className="flex h-full flex-col justify-center p-8 mx-auto max-w-[500px]">
@@ -20,10 +29,12 @@ const Login = () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant={"outline"}>
-          <LogInIcon></LogInIcon>
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant={"outline"}>
+            <LogInIcon></LogInIcon>
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       <div className="relative h-full w-full">
         <Image src={"/login.png"} alt="login" fill className="object-cover" />
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>entrar</Button>
+    <div className="h-full flex items-center justify-center">
+      <UserButton showName />
     </div>
   );
 }
