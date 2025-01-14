0a997cbfe0f4b2f275e82aefe2fae5682ae151d3
Gabriel Barreto <gabriel.devloper@gmail.com>
2025-01-14 20:54:24 -0300

    feat: protect routes auth

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,9 +3,21 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionsColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const Transactions = async () => {
-  const transactions = await db.transacoes.findMany({});
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const transactions = await db.transacoes.findMany({
+    where: {
+      id_usuario: userId,
+    },
+  });
 
   return (
     <>
