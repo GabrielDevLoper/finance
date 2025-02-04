7a9938ce8b570bee0d6429b8b182d47635ef048a
Gabriel Barreto <gabriel.devloper@gmail.com>
2025-02-03 22:52:03 -0300

    fix: generate report with chatgpt, runtime edge test
    #4

--- app/_components/ai-report-button.tsx
+++ app/_components/ai-report-button.tsx
@@ -17,8 +17,8 @@ import { ScrollArea } from "./ui/scroll-area";
 import Markdown from "react-markdown";
 import Link from "next/link";
 import { toast } from "sonner";
-// import { generateAiReport } from "../(home)/_actions/generate-ai-report-with-chatgpt";
-import {generateAiReportWithDeepSeek} from "@/app/(home)/_actions/generate-ai-report-with-deepseek";
+import { generateAiReport } from "../(home)/_actions/generate-ai-report-with-chatgpt";
+// import {generateAiReportWithDeepSeek} from "@/app/(home)/_actions/generate-ai-report-with-deepseek";
 // import { getRelatoriosMensais } from "../_actions/get-report";
 
 interface AiReportButtonProps {
@@ -39,9 +39,7 @@ const AiReportButton = ({
     try {
       setReportIsLoading(true);
 
-      const aiReport = await generateAiReportWithDeepSeek({ month, year });
-
-      console.log(aiReport);
+      const aiReport = await generateAiReport({ month, year });
 
       // Toast de sucesso
       toast.success("Relatório da IA gerado com sucesso ✔️", {
