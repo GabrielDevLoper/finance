8f654131959377dddd2f1b7825f0f17a701afe45
Gabriel Barreto <gabriel.devloper@gmail.com>
2025-02-07 20:22:27 -0300

    fix: update Telegram webhook route to correctly parse and return request body as JSON

--- app/api/webhooks/telegram/route.ts
+++ app/api/webhooks/telegram/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
-  console.log(req.body);
+  const body = await req.json(); // Obtendo o corpo da requisição
 
-  return NextResponse.json({ body: req.json() });
+  return NextResponse.json({ body });
 };
