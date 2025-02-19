2d776e702a8f1c774db5c22538ab412ef7529587
Gabriel Barreto <gabriel.devloper@gmail.com>
2025-02-18 21:16:30 -0300

    fix: update Telegram webhook route to return matched JSON object directly

--- app/api/webhooks/telegram/route.ts
+++ app/api/webhooks/telegram/route.ts
@@ -9,16 +9,16 @@ export const POST = async (req: Request) => {
   const body = await req.text(); // Obtendo o corpo da requisição
 
   // Expressão regular para capturar o JSON dentro do texto
-  const jsonMatch = body.match(/\{[\s\S]*\}/);
+  // const jsonMatch = body.match(/\{[\s\S]*\}/);
 
-  if (!jsonMatch) {
-    return NextResponse.json(
-      { error: "Nenhum JSON encontrado na resposta." },
-      { status: 400 }
-    );
-  }
-  const jsonData = JSON.parse(jsonMatch[0]);
-  return NextResponse.json(jsonData); // Retornando apenas o JSON
+  // if (!jsonMatch) {
+  //   return NextResponse.json(
+  //     { error: "Nenhum JSON encontrado na resposta." },
+  //     { status: 400 }
+  //   );
+  // }
+  // const jsonData = JSON.parse(jsonMatch[0]);
+  return NextResponse.json(body); // Retornando apenas o JSON
 
   // const userListResponse = await clerkClient().users.getUserList({
   //   emailAddress: body.email,
